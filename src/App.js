38514bcde291bb29ccd9e9de53b2b55425475c76
Flavio de Figueiroa <flavio.figueiroats@gmail.com>
2024-10-21 00:47:55 -0300

    test live

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Dashboard from './components/pages/Dashboard';
 import Despesas from './components/pages/Despesas';
